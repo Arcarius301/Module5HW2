71b4274d41b80eb94e14a4e244c9863fcfb7be40
Pavel Kaznachei <pavel.kaznachei@gmail.com>
2022-02-10 16:57:26 +0200

    Add project files.

---
+++ Module5HW2/Controllers/CatalogItemController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Module5HW2.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class CatalogItemController : Controller
+    {
+        private readonly ILogger<CatalogItemController> _logger;
+
+        public CatalogItemController(ILogger<CatalogItemController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet(Name = "GetItem")]
+        public IEnumerable<Item> Get()
+        {
+            return Enumerable.Range(1, 5).Select(index => new Item
+            {
+                Id = index,
+                Name = index.ToString(),
+                Description = "TestDesc",
+                Price = Random.Shared.Next(30, 2001)
+            })
+            .ToArray();
+        }
+    }
+}
---
+++ Module5HW2/Item.cs
@@ -0,0 +1,13 @@
+﻿namespace Module5HW2
+{
+    public class Item
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; } = null!;
+
+        public string Description { get; set; } = null!;
+
+        public decimal Price { get; set; }
+    }
+}
